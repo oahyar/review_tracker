7608269cb6e82a714dc538eab07b2baed94584f2
oahyar <oahyar@gmail.com>
2025-05-30 11:23:29 +0800

    update code

--- tracker.py
+++ tracker.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -18,4 +19,5 @@ def track():
     return redirect(FINAL_URL)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
